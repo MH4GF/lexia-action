a88341810854405e68729151704f87921e76a504
MH4GF <h.miyagi.cnw@gmail.com>
2025-02-24 12:56:18 +0900

    Refactor GitHub Action with improved type safety and module export

--- packages/action/dist/index.js
+++ packages/action/dist/index.js
@@ -27254,8 +27254,9 @@ var coreExports = requireCore();
  */
 async function wait(milliseconds) {
     return new Promise((resolve) => {
-        if (isNaN(milliseconds))
+        if (Number.isNaN(milliseconds)) {
             throw new Error('milliseconds is not a number');
+        }
         setTimeout(() => resolve('done!'), milliseconds);
     });
 }
@@ -27272,22 +27273,17 @@ async function run() {
         coreExports.debug(`Waiting ${ms} milliseconds ...`);
         // Log the current timestamp, wait, then log the new timestamp
         coreExports.debug(new Date().toTimeString());
-        await wait(parseInt(ms, 10));
+        await wait(Number.parseInt(ms, 10));
         coreExports.debug(new Date().toTimeString());
         // Set outputs for other workflow steps to use
         coreExports.setOutput('time', new Date().toTimeString());
     }
     catch (error) {
         // Fail the workflow run if an error occurs
-        if (error instanceof Error)
+        if (error instanceof Error) {
             coreExports.setFailed(error.message);
+        }
     }
 }
 
-/**
- * The entrypoint for the action. This file simply imports and runs the action's
- * main logic.
- */
-/* istanbul ignore next */
-run();
-//# sourceMappingURL=index.js.map
+export { run };
